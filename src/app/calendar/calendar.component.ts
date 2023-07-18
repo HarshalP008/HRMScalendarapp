a362946ada0a2702f4eb2b03bd9ef6bab6b7596a
HarshalP008 <harshalpatel6789@gmail.com>
2023-07-18 13:50:08 +0530

    Status shown

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -27,7 +27,11 @@ export class CalendarComponent implements OnInit{
   currentYear : any = +this.currentDate.getFullYear();
   totalDays !: any;
   datesArr: any = [];
-  month:any=[];
+  showList = false;
+  status : any;
+  month:any;
+  selDate !:string;
+  monthsArr!: any[];
   
   ngOnInit(){
     this.getDays();
@@ -41,28 +45,48 @@ export class CalendarComponent implements OnInit{
   getmonthDays() {
     for (let i = 0; i < this.allMonths.length; i++) {
       this.month = this.allMonths[i];
+      // this.monthsArr.push(this.month);
+      // console.log(this.monthsArr)
       this.totalDays = new Date(this.currentYear, i + 1, 0).getDate();
       console.log(this.totalDays);
       this.month.date= Array.from({ length: this.totalDays }, (_, j) => (j + 1).toString());
       for(let k = 0; k < this.month.date.length; k++){
-        // this.month.day= Array.from({ length: 7}, (_, k) => (k).toString());
-        this.month.days.push(new Date(this.currentYear, i , k + 1).getDay());
+        // this.month.days.push(new Date(this.currentYear, i , k + 1).getDay()); //for day 0123456 this.month.days.push(this.datePipe.transform(new Date(this.currentYear, i , k + 1),"dd-MM-yyyy")?.toString().slice(0,16));
+        let d= this.datePipe.transform(new Date(this.currentYear, i , k + 1),"EEE dd-MM-yyyy ");
+        this.month.days.push(d);
       }
       console.log(this.month);
     }
   }
+  // markHoliday(day:string){
+  //   // let SelDate= new Date(this.currentYear,1,day).getDate();
+  //   let selDate=day.concat("holiday");
+  //   console.log("marked weekend", selDate);
+  //   this.month.holidays= [];
+  //   this.month.holidays.push(selDate);
+  //   console.log(this.month.holidays);
+  // }
   dayColor(day:any){
-    return {'background-color' : (day == 0 || day == 6)? 'lightgreen' : 'smokewhite'}
-  }
-  markHoliday(day:any){
-    let SelDate= new Date(this.currentYear,1,day).getDate();
-    console.log("marked weekend", SelDate);
+    return {'background-color' : ( day.includes("Sat") || day.includes("Sun") || this.status== 'GovtHday') ? 'lightgreen' : 'smokewhite'}
   }
   nextYear() {
+    this.month = '';
     this.currentYear += 1;
     this.getmonthDays();
   }
   prevYear() {
     this.currentYear -= 1;
+  }
+  toggleList(event: MouseEvent) {
+    event.stopPropagation();
+    this.showList = !this.showList;
+  }
+  selectValue(day:any, value: string) {
+    this.status = value;
+    this.showList = !false;
+    this.selDate= day.concat(value);
+    console.log(this.selDate);
+    this.month.days[day.slice(4,6) - 1]= this.selDate;
+    console.log(this.month);
   }
 }
